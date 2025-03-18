180b81d1a940be296ece0ad25c0bf62dd6106bd7
ctalneau <clement.talneau@epitech.eu>
2025-03-18 11:29:55 +0100

    [FEAT](Main menu): Added basic main menu for arcade using sfml

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,22 @@
 ** Main function
 */
 #include <iostream>
+#include <fstream>
+#include "main.hpp"
+
+static bool check_path(const std::string &path)
+{
+    std::ifstream lib(path);
+
+    if (path.size() < 4)
+        return false;
+    if (path.substr(path.size() - 3) != ".so")
+        return false;
+    if (lib.is_open() == false)
+        return false;
+    lib.close();
+    return true;
+}
 
 /**
  * @brief Main function
@@ -16,12 +32,20 @@
  * @param av Arguments
  * @return int Return value
  */
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
     if (ac != 2) {
         std::cerr
             << "Usage: ./arcade path_to_graphical_library.so"
             << std::endl;
         return 84;
     }
+    // if (check_path(av[1]) == false) {
+    //     std::cerr
+    //         << "Error: Invalid path to graphical library"
+    //         << std::endl;
+    //     return 84;
+    // }
+    launch_main_menu();
     return 0;
 }
---
+++ src/main.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** main
+*/
+
+#ifndef MAIN_HPP_
+    #define MAIN_HPP_
+
+    void launch_main_menu(void);
+
+#endif /* !MAIN_HPP_ */
