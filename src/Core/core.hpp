25d031733d533e2fbd36d7892dab0645facb6ecb
ctalneau <clement.talneau@epitech.eu>
2025-03-19 19:09:33 +0100

    [FEAT](ScoreManager): Added ScoreManager (fully theorical as some of the function are not implemented)

--- src/Core/core.hpp
+++ src/Core/core.hpp
@@ -10,6 +10,7 @@
     #define SRC_CORE_CORE_HPP_
     #include <memory>
     #include <string>
+    #include <vector>
     #include "../Shared/Interface/IGameModule.hpp"
     #include "../Shared/Interface/IDisplayModule.hpp"
 
@@ -23,11 +24,15 @@ class Core {
     void loadDisplay(const std::string &displayName);
     void unloadGame();
     void unloadDisplay();
+    void loadAllLibrary();
     void printHelp();
 
  private:
     std::unique_ptr<IGameModule> _currentGame;
     std::unique_ptr<IDisplayModule> _currentDisplay;
+    //! Contains all the library present in ./lib/ and stores them as string
+    //! (they're path) first string is there type (Libe or Game)
+    std::vector<std::pair<const std::string, const std::string>> _allLib;
 };
 }  // namespace Arcade
 #endif  // SRC_CORE_CORE_HPP_
