c160e1605251a812ef7d615d28603765a8f2ca6a
ctalneau <clement.talneau@epitech.eu>
2025-03-21 15:18:18 +0100

    [FEAT](SFML Graphic library): Added SFML Graphic library support / dynamic library

--- src/Graphics/SFML/SFML.cpp
+++ src/Graphics/SFML/SFML.cpp
@@ -6,3 +6,101 @@
 ** SFML
 */
 #include "SFML/SFML.hpp"
+#include <stdexcept>
+#include <memory>
+#include <utility>
+#include <string>
+#include <iostream>
+
+SFML::~SFML() {
+    _window.reset();
+    _fonts.clear();
+    _textures.clear();
+}
+
+void SFML::init(float x, float y) {
+    int width = static_cast<int>(x);
+    int height = static_cast<int>(y);
+    createWindow(width, height);
+    _windowWidth = width;
+    _windowHeight = height;
+}
+
+void SFML::stop() {
+    if (_window && _window->isOpen()) {
+        _window->close();
+    }
+    _window.reset();
+}
+
+void SFML::clearScreen() {
+    if (_window) {
+        _window->clear(sf::Color::Black);
+    }
+}
+
+void SFML::refreshScreen() {
+    if (_window) {
+        _window->display();
+    }
+}
+
+void SFML::drawEntity(int x, int y, char symbol) {
+}
+
+void SFML::drawTexture(int x, int y, const std::string& texturePath) {
+    auto texture = loadTexture(texturePath);
+    if (!texture) {
+        return;
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(*texture);
+    sprite.setPosition(static_cast<float>(x), static_cast<float>(y));
+
+    _window->draw(sprite);
+}
+
+void SFML::drawText(int x, int y, const std::string& text) {
+    auto font = loadFont("assets/fonts/arial.ttf");
+    if (!font)
+        return;
+
+    sf::Text sfText;
+    sfText.setFont(*font);
+    sfText.setString(text);
+    sfText.setCharacterSize(24);
+    sfText.setFillColor(sf::Color::White);
+    sfText.setPosition(static_cast<float>(x), static_cast<float>(y));
+
+    _window->draw(sfText);
+}
+
+void SFML::pollEvents() {
+    sf::Event event;
+    while (_window && _window->pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            _running = false;
+            _window->close();
+        }
+    }
+}
+
+bool SFML::isOpen() const {
+    return _running && _window && _window->isOpen();
+}
+
+extern "C" {
+    __attribute__((constructor))
+    const char* init_sfml(void) {
+        return "Lib";
+    }
+
+    __attribute__((destructor))
+    void fini_sfml(void) {
+    }
+
+    Arcade::IDisplayModule* entryPoint(void) {
+        return new SFML();
+    }
+}
--- src/Graphics/SFML/SFML.hpp
+++ src/Graphics/SFML/SFML.hpp
@@ -8,5 +8,38 @@
 
 #ifndef SRC_GRAPHICS_SFML_SFML_HPP_
     #define SRC_GRAPHICS_SFML_SFML_HPP_
+    #include <SFML/Graphics.hpp>
+    #include <memory>
+    #include <string>
+    #include <unordered_map>
+    #include "../../Shared/Interface/IDisplayModule.hpp"
 
+    class SFML : public Arcade::IDisplayModule {
+     private:
+        std::string _name;
+        std::unique_ptr<sf::RenderWindow> _window;
+        int _windowWidth = 0;
+        int _windowHeight = 0;
+        bool _running = true;
+        void createWindow(int width, int height);
+        sf::Font* loadFont(const std::string& fontPath);
+        sf::Texture* loadTexture(const std::string& texturePath);
+        std::unique_ptr<sf::RenderWindow> _window;
+        std::unordered_map<std::string, std::unique_ptr<sf::Font>> _fonts;
+        std::unordered_map<std::string, std::unique_ptr<sf::Texture>> _textures;
+
+     public:
+        SFML() : _name("SFML") {}
+        ~SFML() override;
+        void init(float width = 800.f, float height = 600.f) override;
+        void stop() override;
+        void clearScreen() override;
+        void refreshScreen() override;
+        void drawEntity(int x, int y, char symbol) override;
+        void drawTexture(int x, int y, const std::string &textureId) override;
+        void drawText(int x, int y, const std::string &text) override;
+        void pollEvents() override;
+        bool isOpen() const override;
+        const std::string& getName() const override { return _name;};
+    };
 #endif  // SRC_GRAPHICS_SFML_SFML_HPP_
