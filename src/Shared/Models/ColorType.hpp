5ec8572bf88109626abe98a6992f1ac2a28a92f3
Samuel Tesson <samuel.tesson@epitech.eu>
2025-03-27 15:23:14 +0100

    [FEAT](Models): add Color/Keys and MouseButton models (enum)

---
+++ src/Shared/Models/ColorType.hpp
@@ -0,0 +1,28 @@
+// Copyright 2025 <Epitech>
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** Key codes for input handling
+*/
+
+#ifndef SRC_SHARED_MODELS_COLORTYPE_HPP_
+    #define SRC_SHARED_MODELS_COLORTYPE_HPP_
+
+namespace Arcade {
+    enum Color {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        MAGENTA,
+        ORANGE,
+        BROWN,
+        GREY,
+        CYAN,
+        WHITE,
+        BLACK
+    };
+}  // namespace Arcade
+
+#endif  // SRC_SHARED_MODELS_COLORTYPE_HPP_
