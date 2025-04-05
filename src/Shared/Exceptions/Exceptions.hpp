e84d66e5ea3039a91edb29335cacfcf84c5849e7
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-04-05 11:33:15 +0200

    [ADD](Exceptions): Create Exceptions.hpp header to include all exception classes for the Arcade project

---
+++ src/Shared/Exceptions/Exceptions.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** Header including all exception classes for the Arcade project
+*/
+
+#ifndef ARCADE_EXCEPTIONS_HPP_
+#define ARCADE_EXCEPTIONS_HPP_
+
+#include "ArcadeException.hpp"
+#include "GameException.hpp"
+#include "GraphicsException.hpp"
+#include "InputException.hpp"
+#include "LibraryLoadException.hpp"
+#include "ResourceException.hpp"
+
+#endif  // ARCADE_EXCEPTIONS_HPP_
