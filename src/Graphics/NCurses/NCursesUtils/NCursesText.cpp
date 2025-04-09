219b6b0e75a2833db74ded236cdd86fcfd132d53
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-04-10 00:47:23 +0200

    [STYLE](NCursesModule): remove comment class definition for improved readability

--- src/Graphics/NCurses/NCursesUtils/NCursesText.cpp
+++ src/Graphics/NCurses/NCursesUtils/NCursesText.cpp
@@ -14,10 +14,8 @@ void NCurses::NCursesText::drawText(WINDOW* window, const std::string &text,
     int x, int y, Arcade::Color color) {
     if (!window)
         return;
-    // Récupère la valeur de la paire de couleurs déjà convertie
     int colorPairValue = NCursesColor::convertColor(color);
     if (colorPairValue)
-        // Passe directement la valeur sans réappliquer COLOR_PAIR
         drawTextWithColor(window, text, x, y, colorPairValue);
     else
         mvwprintw(window, y, x, "%s", text.c_str());
@@ -28,7 +26,6 @@ void NCurses::NCursesText::drawTextWithColor(WINDOW* window,
     int x, int y, int colorPair) {
     if (!window)
         return;
-    // colorPair est déjà le résultat de COLOR_PAIR(), pas besoin de l'appliquer à nouveau
     wattron(window, colorPair);
     mvwprintw(window, y, x, "%s", text.c_str());
     wattroff(window, colorPair);
