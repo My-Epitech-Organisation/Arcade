6b86ba1f102798eef3c34d16b684bb9ba557168a
ctalneau <clement.talneau@epitech.eu>
2025-04-09 18:02:47 +0200

    [FIX](Unit-test): Added unit-test back to fix CI

---
+++ src/Games/Game1/Game1.cpp
@@ -0,0 +1,13 @@
+// Copyright 2025 <Epitech>
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400 Arcade
+** File description:
+** Game1
+*/
+#include "Game1/Game1.hpp"
+#include <iostream>
+
+void Game1::printHelp() {
+    std::cout << "USAGE: ./arcade lib_name" << std::endl;
+}
---
+++ src/Games/Game3/Game3.hpp
@@ -0,0 +1,11 @@
+// Copyright 2025 <Epitech>
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400 Arcade
+** File description:
+** Game3 class
+*/
+#ifndef SRC_GAMES_GAME3_GAME3_HPP_
+    #define SRC_GAMES_GAME3_GAME3_HPP_
+
+#endif  // SRC_GAMES_GAME3_GAME3_HPP_
