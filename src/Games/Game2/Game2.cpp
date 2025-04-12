e930e13280caac7496cf6b5d38f7c01868fe6076
Samuel Tesson <samuel.tesson@epitech.eu>
2025-04-13 00:48:19 +0200

    [FIX] Remove unused games and their associated tests

--- src/Games/Game2/Game2.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2025 <Epitech>
-/*
-** EPITECH PROJECT, 2025
-** B-OOP-400 Arcade
-** File description:
-** Game2
-*/
-#include "Game2/Game2.hpp"
--- src/Games/Game3/Game3.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2025 <Epitech>
-/*
-** EPITECH PROJECT, 2025
-** B-OOP-400 Arcade
-** File description:
-** Game3
-*/
-#include "Game3/Game3.hpp"
